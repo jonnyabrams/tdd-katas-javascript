04848a1c716b95cbc0c79c4de87e7af801c9492f
Jonny Abrams <jonnyabrams@hotmail.com>
2022-05-21 08:02:04 +0100

    Passed 2nd test for autocomplete

--- __test__/autocomplete.test.js
+++ __test__/autocomplete.test.js
@@ -4,4 +4,8 @@ describe('autocomplete', () => {
   it('outputs an array', () => {
     expect(autocomplete('ai', ['airplane','airport','apple','ball'])).toBeInstanceOf(Array);
   });
+
+  it('returns values from the array that start with the input string', () => {
+    expect(autocomplete('ai', ['airplane','airport','apple','ball'])).toEqual(['airplane','airport']);
+  });
 });
